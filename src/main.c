0fac65c9f1fcc7a3e24a128a687e60254c09bf08
Gant Laborde <gantman@gmail.com>
2022-09-07 10:50:10 -0500

    3d projected

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 
 const int N_POINTS = 9 * 9 * 9;
 vec3_t cube_points[N_POINTS];
+vec2_t projected_points[N_POINTS];
+float fov_factor = 120;
 
 bool is_running = false;
 
@@ -57,18 +59,38 @@ void process_input(void) {
   }
 }
 
-void update(void) {
+vec2_t project(vec3_t point) {
+  vec2_t projected_point = {
+    .x = (fov_factor * point.x),
+    .y = (fov_factor * point.y)
+  };
+  return projected_point;
+}
 
+void update(void) {
+  for (int i = 0; i < N_POINTS; i++) {
+    vec3_t point = cube_points[i];
+    vec2_t projected_point = project(point);
+    projected_points[i] = projected_point;
+  }
 }
 
 void render(void) {
-  SDL_SetRenderDrawColor(renderer, 255, 21, 21, 255);
-  SDL_RenderClear(renderer);
 
-  draw_rect(150, 250, 200, 100, random_color());
+  // Loop all projected points and render them
+  for (int i = 0; i < N_POINTS; i++) {
+    vec2_t point = projected_points[i];
+    draw_rect(
+      point.x + (window_width / 2),
+      point.y + (window_height / 2),
+      5,
+      5,
+      0xFFFFFFFF
+    );
+  }
  
   render_color_buffer();
-  uint32_t some_color = 0xFFFFFF00; 
+  uint32_t some_color = 0xFF000000; 
   clear_color_buffer(some_color);
 
   SDL_RenderPresent(renderer);

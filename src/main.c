8e31b002628513d8dd2e6d221488813266d82b09
Gant Laborde <gantman@gmail.com>
2022-09-21 14:53:40 -0500

    scale, rotate, translate

--- src/main.c
+++ src/main.c
@@ -95,10 +95,16 @@ void update(void) {
   mesh.rotation.x += 0.005;
   mesh.rotation.y += 0.003;
   mesh.rotation.z += 0.001;
+  mesh.scale.x += 0.0002;
+  mesh.scale.y += 0.0001;
+  mesh.translation.x += 0.001;
+  mesh.translation.z = 5.0;
 
-  mesh.scale.x += 0.002;
-  mesh.scale.y += 0.001;
   mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
+  mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
+  mat4_t rotation_matrix_x = mat4_make_rotation_x(mesh.rotation.x);
+  mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
+  mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
 
   int num_faces = array_length(mesh.faces);
   for (int i = 0; i < num_faces; i++) {
@@ -116,9 +122,10 @@ void update(void) {
       vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
       transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-
-      // Translate the vertex away from camera
-      transformed_vertex.z += 5;
+      transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
+      transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
+      transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
+      transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
 
       // Saved transformed vertex
       transformed_vertices[j] = transformed_vertex;

20b8de52e39da6f35f70cad8d197760c0d3fe56d
Gant Laborde <gantman@gmail.com>
2022-09-22 11:43:48 -0500

    world matrix

--- src/main.c
+++ src/main.c
@@ -121,11 +121,14 @@ void update(void) {
     for (int j = 0; j < 3; j++) {
       vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-      transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-      transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
-      transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
-      transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
-      transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
+      mat4_t world_matrix = mat4_identity();
+      world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+      world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+      world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+      world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+      world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
+
+      transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
 
       // Saved transformed vertex
       transformed_vertices[j] = transformed_vertex;
